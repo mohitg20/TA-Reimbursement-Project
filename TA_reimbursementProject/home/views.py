682900c6b197dbe66ff3e1b54634f7ac78a16b73
AKHILMANISHJAIN <84617145+AKHILMANISHJAIN@users.noreply.github.com>
2022-03-20 22:40:59 +0530

    ullu

--- TA_reimbursementProject/home/views.py
+++ TA_reimbursementProject/home/views.py
@@ -209,15 +209,16 @@ def application(request):
         application=Application(block_yr=block_yr,email=email,joining=joining,basic_pay=basic_pay,Name=Name,Designation=Designation,section=section,avail=avail,duration=duration,departure=departure,nature=nature,Purpose=Purpose,place=place,place1=place1,address=address,mode=mode,Name1=Name1,Age1=Age1,Name2=Name2,Age2=Age2,Name3=Name3,Age3=Age3,advance=advance)
         application.save()
         return render(request,'status.html')
-    # if User_profile.objects.filter(email=request.user.email).exists():
-    #     plz=User_profile.objects.get(email=request.user.email)
-    #     # print(plz.email)
-    #     return render(request,'user_profile.html',context={'userdata':plz})
-    # else:
-    #     return render(request,'user_profileBase.html',context={'userdata':request.user})
+    if Application.objects.filter(email=request.user.email).exists():
+        plz=Application.objects.get(email=request.user.email)
+        # print(plz.email)
+        return render(request,'application.html',context={'userdata':plz})
+    else:
+        return render(request,'applicationBase.html',context={'userdata':request.user})
     return render(request,'application.html')
 
 # def pending_requests(request):
+
 #     plz=Application.objects.get(email="user@iitk.ac.in")
 #     print(plz.email)
 #     # return render(request,'pending.html',{'AppData':plz})
